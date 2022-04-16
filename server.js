d3b4d58f3d2d8b456b9c2777a31f7259b538ce2f
katikn <antoshapetlin@gmail.com>
2022-04-16 21:01:52 +0700

    Работа с handlebars и db

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
+const hbs = require('hbs');
 
 let app = express();
 app.set('view engine', 'hbs');
 app.set('views', './templates');
 
 app.use(express.static(__dirname + '/static'));
+hbs.registerPartials(__dirname + '/templates/partials')
 
 async function data(query, datas) {
     let db = new sqlite3.Database('blog.db', (err) => {
@@ -16,7 +18,13 @@ async function data(query, datas) {
         }
     })
     let sqls = {
-        first: "SELECT * FROM general"
+        main: "SELECT * FROM general",
+        life_category: "SELECT * FROM general WHERE category = ?",
+        popular_category: "SELECT * FROM general WHERE category = ?",
+        travel_category: "SELECT * FROM general WHERE category = ?",
+        technology_category: "SELECT * FROM general WHERE category = ?",
+        education_category: "SELECT * FROM general WHERE category = ?",
+        covid_category: "SELECT * FROM general WHERE category = ?",
     };
     let sql = sqls[query];
 
@@ -39,22 +47,98 @@ async function data(query, datas) {
 
 
 app.get('/', (req, res) => {
-    data('first', []).then((data) => {
+    data('main', []).then((data) => {
         data_template = {
             headers: Object.keys(data[0]),
-            querys: data
+            querys: data.slice(4),
+            most_recent: data[0],
+            featured1: data[1],
+            featured2: data[2],
+            featured3: data[3]
         };
         res.render('index.hbs', data_template)
     });
 });
 
+app.get('/life', (req, res) => {
+    data('life_category', [6]).then((data) => {
+        data_template = {
+            headers: Object.keys(data[0]),
+            querys: data.slice(2),
+            most_recent: data.slice(0, 2)
+        };
+        res.render('categories.hbs', data_template)
+    });
+});
+
+app.get('/popular', (req, res) => {
+    data('popular_category', [1]).then((data) => {
+        data_template = {
+            headers: Object.keys(data[0]),
+            querys: data.slice(2),
+            most_recent: data.slice(0, 2)
+        };
+        res.render('categories.hbs', data_template)
+    });
+});
+
+app.get('/travel', (req, res) => {
+    data('travel_category', [2]).then((data) => {
+        data_template = {
+            headers: Object.keys(data[0]),
+            querys: data.slice(2),
+            most_recent: data.slice(0, 2)
+        };
+        res.render('categories.hbs', data_template)
+    });
+});
+
+app.get('/technology', (req, res) => {
+    data('technology_category', [4]).then((data) => {
+        data_template = {
+            headers: Object.keys(data[0]),
+            querys: data.slice(2),
+            most_recent: data.slice(0, 2)
+        };
+        res.render('categories.hbs', data_template)
+    });
+});
+
+app.get('/education', (req, res) => {
+    data('education_category', [5]).then((data) => {
+        data_template = {
+            headers: Object.keys(data[0]),
+            querys: data.slice(2),
+            most_recent: data.slice(0, 2)
+        };
+        res.render('categories.hbs', data_template)
+    });
+});
+
+app.get('/covid', (req, res) => {
+    data('covid_category', [3]).then((data) => {
+        data_template = {
+            headers: Object.keys(data[0]),
+            querys: data.slice(2),
+            most_recent: data.slice(0, 2)
+        };
+        res.render('categories.hbs', data_template)
+    });
+});
+
+app.get('/life/:id', (req, res) => {
+    data('life_category', [6]).then((data) => {
+        data_template = {
+            headers: Object.keys(data[0]),
+            querys: data
+        };
+        res.render('inner.hbs', data_template)
+    });
+});
+
+
 app.get('/inner', (req, res) => {
     res.render('inner.hbs')
 });
-
-app.get('/category', (req, res) => {
-    res.render('categories.hbs')
-});
-
 
 app.listen(3000);
